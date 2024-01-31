f54a7ab241bc4ec34a4f7f43c09fb4ce443b696e
Ryan Oliveira Brito <ryanoliveirabrito178@gmail.com>
2024-01-31 18:01:02 -0300

    add zod validations

--- components/admin/geral/geral.tsx
+++ components/admin/geral/geral.tsx
@@ -8,32 +8,71 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { ProviderUserConfig } from "@/components/provider-user-config";
 import AvatarUploader from "@/services/user/avatar/change-avatar";
 import { Button } from "@/components/ui/button";
-import { Contact } from "lucide-react";
+import * as z from "zod";
 import { PiWarningCircle } from "react-icons/pi";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const loginFormSchema = z.object({
+  email: z.string().optional(), // Tornando o campo de e-mail opcional
+  contact: z
+    .string()
+    .regex(/^\d{11}$/, {
+      message: "O telefone deve conter exatamente 11 dígitos",
+    })
+    .optional(),
+  username: z
+    .string()
+    .max(12) // Limitando a 12 caracteres
+
+    .optional(), // Tornando o campo de username opcional
+  preferred_currency: z.any().optional(), // Tornando o campo de moeda preferida opcional
+});
+
+type LoginFormInputs = z.infer<typeof loginFormSchema>;
 export const GeralConfig = () => {
   const { data, loading, fetchUserData } = useUserProfile();
-  const [newUsername, setNewUsername] = useState("");
-  const [newContact, setNewContact] = useState("");
-  const [newCurrency, setNewCurrency] = useState("");
-  const [newEmail, setNewEmail] = useState("");
-  const [changesMade, setChangesMade] = useState(false);
   const { toast } = useToast();
+  const [isInputFocused, setIsInputFocused] = useState(false);
 
+  const handleInputFocus = () => {
+    setIsInputFocused(true);
+  };
+
+  const handleInputBlur = () => {
+    setIsInputFocused(false);
+  };
   const token = Cookies.get("auth_token");
+  const [formData, setFormData] = useState<LoginFormInputs>({
+    email: "",
+    contact: "",
+    username: "",
+    preferred_currency: "",
+  });
+
+  const [isSubmitDisabled, setIsSubmitDisabled] = useState(true); // Estado para rastrear se o botão de envio deve ser desabilitado
+
+  useEffect(() => {
+    // Verificar se algum campo obrigatório está preenchido
+    const hasRequiredFields = Object.values(formData).some(
+      (value) => value !== ""
+    );
+    setIsSubmitDisabled(!hasRequiredFields);
+  }, [formData]);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormInputs>({
+    resolver: zodResolver(loginFormSchema),
+  });
 
   const handleUpdateProfile = () => {
-    const updatedData = {
-      username: newUsername || data?.username,
-      contact: newContact || data?.contact,
-      preferred_currency: newCurrency || data?.preferred_currency,
-    };
-
     axios
-      .put(
-        "https://codeui-api-production.up.railway.app/api/user",
-        updatedData,
-        { headers: { Authorization: `Bearer ${token}` } }
-      )
+      .put("https://codeui-api-production.up.railway.app/api/user", formData, {
+        headers: { Authorization: `Bearer ${token}` },
+      })
       .then((response) => {
         console.log("Perfil atualizado com sucesso:", response.data);
         toast({
@@ -41,7 +80,6 @@ export const GeralConfig = () => {
           title: "Sucesso, perfil atualizado!",
           description: "Perfil atualizado com sucesso.",
         });
-        setChangesMade(false); // Reset changesMade state after successful update
       })
       .catch((error) => {
         console.error("Erro ao atualizar perfil:", error);
@@ -53,33 +91,10 @@ export const GeralConfig = () => {
       });
   };
 
-  useEffect(() => {
-    const interval = setInterval(fetchUserData, 5000);
-    return () => clearInterval(interval);
-  }, []);
-
-  // Function to check if any changes were made
-  useEffect(() => {
-    if (
-      newUsername !== "" ||
-      newContact !== "" ||
-      newCurrency !== ""
-    ) {
-      setChangesMade(true);
-    } else {
-      setChangesMade(false);
-    }
-  }, [newUsername, newContact, newCurrency]);
+  console.log(errors);
 
   return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        if (changesMade) { // Only submit if changes were made
-          handleUpdateProfile();
-        }
-      }}
-    >
+    <form onSubmit={handleSubmit(handleUpdateProfile)}>
       <section className="flex flex-col gap-10">
         {loading ? (
           <div className="flex flex-col gap-5">
@@ -90,22 +105,25 @@ export const GeralConfig = () => {
         ) : (
           <>
             <ProviderUserConfig
-              title="Avatar"
-              subTitle="Este é o seu avatar."
-              description="Clique no avatar para fazer upload de um personalizado de seus arquivos."
-              footer="Um avatar é opcional, mas altamente recomendado."
-              avatar={<AvatarUploader />}
-            />
-            <ProviderUserConfig
               title="Nome de exibição"
               description="Digite seu nome completo ou um nome de exibição com o qual você se sinta confortável."
               footer="Use no máximo 32 caracteres."
               content={
                 <Input
-                  className="w-72"
+                  className={`w-72 outline-none ${
+                    errors.username
+                      ? "outline-none focus-visible:outline-red-500"
+                      : ""
+                  }`}
                   placeholder={data?.username}
-                  value={newUsername}
-                  onChange={(e) => setNewUsername(e.target.value)}
+                  {...register("username")}
+                  value={formData.username}
+                  onChange={(e) =>
+                    setFormData((prevData) => ({
+                      ...prevData,
+                      username: e.target.value,
+                    }))
+                  }
                 />
               }
             />
@@ -115,10 +133,23 @@ export const GeralConfig = () => {
               footer="Use no máximo 11 caracteres."
               content={
                 <Input
-                  className="w-72"
+                  className={`w-72 outline-none${
+                    errors.contact
+                      ? "outline-none focus-visible:outline-red-500"
+                      : ""
+                  } ${isInputFocused ? "text-primary" : "text-muted-foreground"}`}
+                  onFocus={() => setIsInputFocused(true)}
                   placeholder={data?.contact}
-                  value={newContact}
-                  onChange={(e) => setNewContact(e.target.value)}
+                  {...register("contact")}
+                  value={
+                    formData.contact === "" ? data?.contact : formData.contact
+                  }
+                  onChange={(e) =>
+                    setFormData((prevData) => ({
+                      ...prevData,
+                      contact: e.target.value,
+                    }))
+                  }
                 />
               }
             />
@@ -130,8 +161,14 @@ export const GeralConfig = () => {
                 <Input
                   className="w-72"
                   placeholder={data?.preferred_currency}
-                  value={newCurrency}
-                  onChange={(e) => setNewCurrency(e.target.value)}
+                  {...register("preferred_currency")}
+                  value={formData.preferred_currency}
+                  onChange={(e) =>
+                    setFormData((prevData) => ({
+                      ...prevData,
+                      preferred_currency: e.target.value,
+                    }))
+                  }
                 />
               }
             />
@@ -141,11 +178,17 @@ export const GeralConfig = () => {
               footer={`Email atual: ${data?.email}`}
               content={
                 <Input
+                  disabled
                   className="w-72"
                   placeholder={data?.email}
-                  value={newEmail}
-                  onChange={(e) => setNewEmail(e.target.value)}
-                  disabled
+                  {...register("email")}
+                  value={formData.email}
+                  onChange={(e) =>
+                    setFormData((prevData) => ({
+                      ...prevData,
+                      email: e.target.value,
+                    }))
+                  }
                 />
               }
             />
@@ -154,20 +197,23 @@ export const GeralConfig = () => {
       </section>
       <div className="w-full flex border-2 mt-4 rounded-md">
         <div className="flex w-full flex-row items-center flex-1 justify-between py-3 px-4 text-sm">
-         <div className="flex flex-row gap-5 items-center">
-         <PiWarningCircle size={35} color="#fb6376"/>
-          <h1 className="text-muted-foreground mr-5">
-            Ao confirmar as alterações acima, <span className="text-primary font-semibold">você estará modificando as
-            informações associadas à sua conta</span>. Essas mudanças podem afetar
-            diretamente a configuração e o funcionamento da plataforma.
-          </h1>
-         </div>
+          <div className="flex flex-row gap-5 items-center">
+            <PiWarningCircle size={35} color="#fb6376" />
+            <h1 className="text-muted-foreground mr-5">
+              Ao confirmar as alterações acima,{" "}
+              <span className="text-primary font-semibold">
+                você estará modificando as informações associadas à sua conta
+              </span>
+              . Essas mudanças podem afetar diretamente a configuração e o
+              funcionamento da plataforma.
+            </h1>
+          </div>
           <section className="rounded-md">
             <Button
               variant="default"
               className="w-36"
               type="submit"
-              disabled={!changesMade} // Disable button if no changes were made
+              disabled={isSubmitDisabled}
             >
               Confirmar
             </Button>
